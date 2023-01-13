74bf9847dcc74a12357148c49493cacb40986bbd
Kaan Arslan <kaanarslan35@gmail.com>
2023-01-13 09:50:51 +0300

    server side function created

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,15 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 import Results from '../components/Results'
+import requests from '../utils/requests'
 
 
 
 
 
 
-export default function Home() {
+export default function Home({results}) {
+  console.log(results)
   return (
     <>
       <Head>
@@ -26,9 +28,23 @@ export default function Home() {
 
 
       {/* Results */}
-      <Results/>
+      <Results results={results}/>
 
 
     </>
   )
 }
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre || "fetchTrending";
+  const request = await fetch(`
+  https://api.themoviedb.org/3${requests[genre].url}
+  `).then((response) => response.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
+}
+  
